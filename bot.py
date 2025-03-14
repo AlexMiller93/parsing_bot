d82339339e293978dae2dc7e0d8b24a853d91792
AlexMiller93 <alexandr.miller93@gmail.com>
2025-03-14 21:55:13 +0300

    рефакторинг кода, тестирование

--- bot.py
+++ bot.py
@@ -1,19 +1,32 @@
+import logging
 import os
 import sqlite3
 import pandas as pd
 
-from telegram import KeyboardButton, ReplyKeyboardMarkup, Update
+from telegram import (
+    InlineKeyboardButton, 
+    InlineKeyboardMarkup, 
+    Update
+    )
+
 from telegram.ext import (
     ContextTypes, 
     ApplicationBuilder, 
     CommandHandler, 
     MessageHandler,
+    CallbackQueryHandler,
     filters
     )
 
 from dotenv import load_dotenv
 
 load_dotenv()
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 # Подключение к базе данных SQLite
@@ -31,36 +44,42 @@ def init_db():
     conn.commit()
     return conn
 
+
 # Обработчик команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    button = KeyboardButton("Загрузить файл")
-    keyboard = ReplyKeyboardMarkup([[button]], resize_keyboard=True)
+    keyboard = [
+        [InlineKeyboardButton("Загрузить файл Excel", callback_data='upload_file')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text(
-        "Привет! Загрузите файл Excel с данными о сайтах для парсинга.", reply_markup=keyboard)
+        "Привет! Нажмите кнопку ниже, чтобы загрузить файл Excel со столбцами title, url и xpath.", 
+        reply_markup=reply_markup)
 
+# Обработчик нажатия на кнопку
+async def button_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()  
+    await query.edit_message_text(text="Пожалуйста, прикрепите файл Excel.")
 
 # Обработчик получения файла
 async def handle_document(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
-        # file = update.message.document.get_file()
-        # file_path = f"downloads/{update.message.document.file_id}.xlsx"
-
-        # print(file)
-
-        # # Создаем папку для загрузок, если она не существует
-        # if not os.path.exists('downloads'):
-        #     os.makedirs('downloads')
-
-        # await file.download_to_drive(file_path)
-
         file = await context.bot.get_file(update.message.document)
 
-        print(f'File: {file}')
+        # print(f'File: {file}')
         file_path = f"downloads/{update.message.document.file_id}.xlsx"
         await file.download_to_drive(file_path)
 
         # Открываем файл с помощью pandas
         df = pd.read_excel(file_path)
+
+        # Проверка наличия необходимых столбцов
+        required_columns = ['title', 'url', 'xpath']  
+        missing_columns = [col for col in required_columns if col not in df.columns]
+
+        if missing_columns:
+            await update.message.reply_text(f"Ошибка: отсутствуют необходимые столбцы: {', '.join(missing_columns)}.")
+            return
 
         # Выводим содержимое файла
         response = df.to_string(index=False)
@@ -78,13 +97,13 @@ async def handle_document(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     except Exception as e:
         await update.message.reply_text(f"Произошла ошибка: {str(e)}")
-        print(str(e))
 
 
 def main():
     application = ApplicationBuilder().token(TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CallbackQueryHandler(button_callback, pattern='upload_file'))
     application.add_handler(MessageHandler(filters.Document.ALL, handle_document))
 
     application.run_polling()
